3686a0a876e38861b606f7634df195faada6fca5
pivendanila <piven.danila@mail.ru>
2024-06-21 05:57:25 +0300

    Update booking.js

--- JS/booking.js
+++ JS/booking.js
@@ -1,3 +1,4 @@
+
 (function(k,o,t,e,l){
     l = document.createElement("script");
     l.type = "text/javascript";
@@ -30,30 +31,6 @@
             container: "WidgetHorizontalBlockId"
         }
     });
-});
-(function(k,o,t,e,l){
-    l = document.createElement("script");
-    l.type = "text/javascript";
-    l.src = "https://bookonline24.ru/widget.js";
-    l.async = !0;
-    l.onload = l.onreadystatechange = function() {
-        e = this.readyState;
-        !o && (!e || e === "complete") && (o = 1) && k();
-    };
-    t = document.getElementsByTagName("script")[0];
-    t.parentNode.insertBefore(l, t);
-})(function(){
-    HotelWidget.init({
-        // ID организации; ID html элемента нужно проставить ниже в поле container
-        hotelId: "48df3784-4a11-4764-a345-88b64bdc2ffc",
-        version: "2",
-        baseUrl: "https://bookonline24.ru/",
-        hooks: {
-            onError: function(e) { console.error("onError", e); },
-            onInit: function() { console.log("onInit"); },
-            onBooking: function(v) { console.log("onBooking", v) }
-        }
-    }); 
 
     <!-- Просмотр календаря доступности — вертикальный блок -->
     HotelWidget.add({
